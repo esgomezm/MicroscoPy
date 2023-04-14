dc2628b5808025c72aed24b406b4a5a730634623
Iván Hidalgo <ivanhcenalmor@gmail.com>
2023-04-14 17:07:36 +0100

    Change

--- testing_models.py
+++ testing_models.py
@@ -2,7 +2,6 @@ from src.trainers import *
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
 os.environ["CUDA_VISIBLE_DEVICES"] = "2";
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = "1"; 
 
 dataset_config = {'EM': {'data_paths': [None, 'train', None, None, None, 'test'],
                          'crappifier': 'em_AG_D_sameas_preprint',
