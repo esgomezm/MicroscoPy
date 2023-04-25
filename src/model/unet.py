870d7d8dcb1b682773d0ea2937b98b1f88f432d0
Iván Hidalgo <ivanhcenalmor@gmail.com>
2023-04-25 15:55:09 +0100

    U-Net modified to accept scale 1

--- src/model/unet.py
+++ src/model/unet.py
@@ -111,12 +111,14 @@ def preUNet4(output_channels, filters=16, input_size = (128,128,1), upsampling_f
 
   inputs = Input( input_size )
   
-  s = upsample( inputs, out_channels=1, method=upsample_method,
-                     upsampling_factor=upsampling_factor,
-                     input_shape=(input_size[0], input_size[1], input_size[2]))
-
-
-  conv1 = Conv2D(filters, (3,3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(s)
+  if upsampling_factor > 1:
+    s = upsample(inputs, out_channels=1, method=upsample_method,
+                 upsampling_factor=upsampling_factor,
+                 input_shape=(input_size[0], input_size[1], input_size[2]))
+    conv1 = Conv2D(filters, (3,3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(s)
+  else:
+    conv1 = Conv2D(filters, (3,3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    
   conv1 = SpatialDropout2D(0.1)(conv1) if spatial_dropout else Dropout(0.1) (conv1)
   conv1 = Conv2D(filters, (3,3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
   pool1 = AveragePooling2D(pool_size=(2, 2))(conv1)
@@ -223,9 +225,10 @@ def postUNet4(output_channels, filters=16, input_size = (128,128,1), upsampling_
   conv9 = SpatialDropout2D(0.1)(conv9) if spatial_dropout else Dropout(0.1)(conv9)
   conv9 = Conv2D(filters, (3,3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
 
-  conv9 = upsample( conv9, out_channels=1, method=upsample_method,
-                    upsampling_factor=upsampling_factor,
-                    input_shape=(input_size[0], input_size[1], input_size[2]))
+  if upsampling_factor > 1:
+    conv9 = upsample( conv9, out_channels=1, method=upsample_method,
+                        upsampling_factor=upsampling_factor,
+                        input_shape=(input_size[0], input_size[1], input_size[2]))
 
   #outputs = Conv2D(1, (1, 1), activation='sigmoid') (conv9)
   outputs = Conv2D(output_channels, (1, 1)) (conv9)
@@ -321,20 +324,26 @@ def preResUNet(image_shape, output_channels, activation='elu', kernel_initialize
 
     inputs = Input((None, None, image_shape[2]))
 
-    s = upsample( inputs, out_channels=numInitChannels, method=upsample_method,
-                  upsampling_factor=upsampling_factor,
-                  input_shape=(image_shape[0], image_shape[1], image_shape[2]))
+    if upsampling_factor > 1:
+        s = upsample( inputs, out_channels=numInitChannels, method=upsample_method,
+                    upsampling_factor=upsampling_factor,
+                    input_shape=(image_shape[0], image_shape[1], image_shape[2]))
 
-    conv_strides = (1,1) if maxpooling else (2,2)
+        conv_strides = (1,1) if maxpooling else (2,2)
 
-    x = level_block(s, depth, numInitChannels, 3, activation, kernel_initializer,
-                    dropout_value, batchnorm, conv_strides, separable, True,
-                    maxpooling)
+        x = level_block(s, depth, numInitChannels, 3, activation, kernel_initializer,
+                        dropout_value, batchnorm, conv_strides, separable, True,
+                        maxpooling)
 
-    #outputs = Conv2D(1, (1, 1), activation='sigmoid') (x)
+        #outputs = Conv2D(1, (1, 1), activation='sigmoid') (x)
 
-    x = Add()([s,x]) # long shortcut
+        x = Add()([s,x]) # long shortcut
+    else:
+        conv_strides = (1,1) if maxpooling else (2,2)
 
+        x = level_block(s, depth, numInitChannels, 3, activation, kernel_initializer,
+                        dropout_value, batchnorm, conv_strides, separable, True,
+                        maxpooling)
     outputs = Conv2D(output_channels, (1, 1), activation=final_activation) (x)
 
     model = Model(inputs=[inputs], outputs=[outputs])
@@ -376,11 +385,10 @@ def postResUNet(image_shape, output_channels, activation='elu', kernel_initializ
     x = level_block(inputs, depth, numInitChannels, 3, activation, kernel_initializer,
                     dropout_value, batchnorm, conv_strides, separable, True,
                     maxpooling)
-
-    x = upsample( x, out_channels=numInitChannels, method=upsample_method,
-                  upsampling_factor=upsampling_factor,
-                  input_shape=(image_shape[0], image_shape[1], image_shape[2]))
-    
+    if upsampling_factor > 1:
+        x = upsample( x, out_channels=numInitChannels, method=upsample_method,
+                    upsampling_factor=upsampling_factor,
+                    input_shape=(image_shape[0], image_shape[1], image_shape[2]))
 
     #outputs = Conv2D(1, (1, 1), activation='sigmoid') (x)
     outputs = Conv2D(output_channels, (1, 1), activation=final_activation) (x)
@@ -388,164 +396,3 @@ def postResUNet(image_shape, output_channels, activation='elu', kernel_initializ
     model = Model(inputs=[inputs], outputs=[outputs])
 
     return model
-
-def preAttention_U_Net_2D(image_shape = (None,None,1), activation='elu', feature_maps=[16, 32, 64, 128, 256],
-                       drop_values=[0.1,0.1,0.2,0.2,0.3], spatial_dropout=False, batch_norm=False,
-                       k_init='he_normal',output_channels=1,pre_load_weights=False,pretrained_model=None,
-                       train_encoder=True,bottleneck_train=True,skip_connection_train=True,
-                       upsampling_factor=2, upsample_method='UpSampling2D'):
-    """Create pre-upsampling 2D U-Net with Attention blocks.
-       Based on `Attention U-Net: Learning Where to Look for the Pancreas <https://arxiv.org/abs/1804.03999>`_.
-       Parameters
-       ----------
-       image_shape : 2D tuple
-           Dimensions of the input image.
-       activation : str, optional
-           Keras available activation type.
-       feature_maps : array of ints, optional
-           Feature maps to use on each level.
-       drop_values : float, optional
-           Dropout value to be fixed. If no value is provided the default behaviour will be to select a piramidal value
-           starting from ``0.1`` and reaching ``0.3`` value.
-       spatial_dropout : bool, optional
-           Use spatial dropout instead of the `normal` dropout.
-       batch_norm : bool, optional
-           Make batch normalization.
-       k_init : string, optional
-           Kernel initialization for convolutional layers.
-       output_channels: int, optional
-           Number of classes.
-       Returns
-       -------
-       model : Keras model
-           Model containing the Attention U-Net.
-       Example
-       -------
-       Calling this function with its default parameters returns the following network:
-       .. image:: ../img/unet.png
-           :width: 100%
-           :align: center
-       Image created with `PlotNeuralNet <https://github.com/HarisIqbal88/PlotNeuralNet>`_.
-       That networks incorporates in skip connecions Attention Gates (AG), which
-       can be seen as follows:
-       .. image:: ../img/attention_gate.png
-           :width: 100%
-           :align: center
-       Image extracted from `Attention U-Net: Learning Where to Look for the Pancreas <https://arxiv.org/abs/1804.03999>`_.
-    """
-
-    if len(feature_maps) != len(drop_values):
-        raise ValueError("'feature_maps' dimension must be equal 'drop_values' dimension")
-    depth = len(feature_maps)-1
-
-    inputs = Input((None, None, image_shape[2]))
-
-    x = upsample( inputs, out_channels=num_outputs, method=upsample_method,
-                  upsampling_factor=upsampling_factor,
-                  input_shape=(image_shape[0], image_shape[1], image_shape[2]))
-    
-    # List used to access layers easily to make the skip connections of the U-Net
-    l=[]
-
-    # ENCODER
-    for i in range(depth):
-        x = Conv2D(feature_maps[i], (3, 3), activation=None, kernel_initializer=k_init, padding='same',trainable=train_encoder) (x)
-        x = BatchNormalization(trainable=train_encoder) (x) if batch_norm else x
-        x = Activation(activation,trainable=train_encoder) (x)
-        if drop_values is not None:
-            if spatial_dropout:
-                x = SpatialDropout2D(drop_values[i],trainable=train_encoder) (x)
-            else:
-                x = Dropout(drop_values[i],trainable=train_encoder) (x)
-        x = Conv2D(feature_maps[i], (3, 3), activation=None, kernel_initializer=k_init, padding='same',trainable=train_encoder) (x)
-        x = BatchNormalization(trainable=train_encoder) (x) if batch_norm else x
-        x = Activation(activation,trainable=train_encoder) (x)
-
-        l.append(x)
-
-        x = MaxPooling2D((2, 2),trainable=train_encoder)(x)
-      
-    
-    # BOTTLENECK
-    x = Conv2D(feature_maps[depth], (3, 3), activation=None, kernel_initializer=k_init, padding='same',trainable=bottleneck_train)(x)
-    x = BatchNormalization(trainable=bottleneck_train) (x) if batch_norm else x
-    x = Activation(activation,trainable=bottleneck_train) (x)
-    if drop_values is not None:
-            if spatial_dropout:
-                x = SpatialDropout2D(drop_values[depth],trainable=bottleneck_train) (x)
-            else:
-                x = Dropout(drop_values[depth],trainable=bottleneck_train) (x)
-    x = Conv2D(feature_maps[depth], (3, 3), activation=None, kernel_initializer=k_init, padding='same',trainable=bottleneck_train) (x)
-    x = BatchNormalization(trainable=bottleneck_train) (x) if batch_norm else x
-    x = Activation(activation,trainable=bottleneck_train) (x)
-   
-    # DECODER
-    for i in range(depth-1, -1, -1):
-        x = Conv2DTranspose(feature_maps[i], (2, 2), strides=(2, 2), padding='same') (x)
-        attn = AttentionBlock(x, l[i], feature_maps[i], batch_norm,trainable=skip_connection_train)
-        x = concatenate([x, attn])
-        x = Conv2D(feature_maps[i], (3, 3), activation=None, kernel_initializer=k_init, padding='same') (x)
-        x = BatchNormalization() (x) if batch_norm else x
-        x = Activation(activation) (x)
-        if drop_values is not None:
-            if spatial_dropout:
-                x = SpatialDropout2D(drop_values[i]) (x)
-            else:
-                x = Dropout(drop_values[i]) (x)
-
-        x = Conv2D(feature_maps[i], (3, 3), activation=None, kernel_initializer=k_init, padding='same') (x)
-        x = BatchNormalization() (x) if batch_norm else x
-        x = Activation(activation) (x)
-
-
-    outputs = Conv2D( output_channels, (1, 1), activation='linear') (x)
-    '''
-    if num_outputs==1:
-         outputs = Conv2D( num_outputs, (1, 1), activation='sigmoid') (x)
-    else:
-         outputs = Conv2D( num_outputs, (1, 1), activation='softmax') (x)
-    '''
-    
-
-    model = Model(inputs=[inputs], outputs=[outputs])
-    if pre_load_weights:
-        #Loading weights layer by layer except from the last layer whose structure would change 
-    
-        for i in range((len(model.layers)-1)):
-            model.get_layer(index=i).set_weights(pretrained_model.get_layer(index=i).get_weights())
-
-    return model
-
-
-def AttentionBlock(x, shortcut, filters, batch_norm,trainable=False):
-    """Attention block.
-       Extracted from `Kaggle <https://www.kaggle.com/c/tgs-salt-identification-challenge/discussion/64367>`_.
-       Parameters
-       ----------
-       x : Keras layer
-           Input layer.
-       shortcut : Keras layer
-           Input skip connection.
-       filters : int
-           Feature maps to define on the Conv layers.
-       batch_norm : bool, optional
-           To use batch normalization.
-       Returns
-       -------
-       out : Keras layer
-           Last layer of the Attention block.
-    """
-    g1 = Conv2D(filters, kernel_size = 1,trainable=trainable)(shortcut)
-    g1 = BatchNormalization(trainable=trainable) (g1) if batch_norm else g1
-    x1 = Conv2D(filters, kernel_size = 1,trainable=trainable)(x)
-    x1 = BatchNormalization(trainable=trainable) (x1) if batch_norm else x1
-
-    g1_x1 = Add(trainable=trainable)([g1,x1])
-    psi = Activation('relu',trainable=trainable)(g1_x1)
-    psi = Conv2D(1, kernel_size = 1,trainable=trainable)(psi)
-    psi = BatchNormalization(trainable=trainable) (psi) if batch_norm else psi
-    psi = Activation('sigmoid',trainable=trainable)(psi)
-    x = Multiply(trainable=trainable)([x,psi])
-    
-    return x    
-
