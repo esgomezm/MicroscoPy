2e0b7f6542ae7f4a6d9c07a26184a45305d89605
Iván Hidalgo <ivanhcenalmor@gmail.com>
2023-04-26 16:10:26 +0100

    Changed testing config

--- testing_models.py
+++ testing_models.py
@@ -9,7 +9,7 @@ model_config = load_yaml('./general_configs/model_configuration.yaml')
 
 train_config = None
 
-for dataset_name in ['LiveFActinDataset']:#['F-actin', 'ER', 'MT', 'LiveFActinDataset', 'MT-SMLM_all']:
+for dataset_name in ['EM', 'MitoTracker_small', 'F-actin', 'ER', 'MT', 'LiveFActinDataset']: #, 'MT-SMLM_all']:
 
     train_lr, train_hr, val_lr, val_hr, test_lr, test_hr = dataset_config[dataset_name]['data_paths']
 
@@ -21,7 +21,7 @@ for dataset_name in ['LiveFActinDataset']:#['F-actin', 'ER', 'MT', 'LiveFActinDa
     test_lr_path = os.path.join(dataset_root, dataset_name, test_lr) if test_lr is not None else None
     test_hr_path = os.path.join(dataset_root, dataset_name, test_hr) if test_hr is not None else None
 
-    for model_name in ['rcan']: #['unet', 'rcan', 'dfcan', 'wdsr', 'wgan', 'esrganplus']:
+    for model_name in ['unet']: #['unet', 'rcan', 'dfcan', 'wdsr', 'wgan', 'esrganplus']:
 
         if train_config is None:
             test_metric_indexes = [69,  7, 36, 75, 74, 30, 12, 42, 87, 0]
@@ -39,44 +39,48 @@ for dataset_name in ['LiveFActinDataset']:#['F-actin', 'ER', 'MT', 'LiveFActinDa
             discriminator_lr = 0.001
             additional_folder = ""
 
-            # Update the patience to be equal to the number of epochs
-            model_config['optim']['early_stop']['patience'] = number_of_epochs
+            for batch_szie in [1,2,4]:
+                for number_of_epochs in [5,10,20]:
+                    for lr in [0.001, 0.005, 0.0005]:
 
-            validation_split = 0.1
-            data_augmentation = ['rotation', 'horizontal_flip', 'vertical_flip']
-            datagen_sampling_pdf = 0
+                        # Update the patience to be equal to the number of epochs
+                        model_config['optim']['early_stop']['patience'] = number_of_epochs
 
-            train_config = {'model':model_name,
-                        'dataset_name':dataset_name,
-                        'optimizer':optimizer,
-                        'discriminator_optimizer':discriminator_optimizer,
-                        'scheduler':scheduler,
-                        'discriminator_lr_scheduler':discriminator_lr_scheduler,
-                        'seed':seed,
-                        'batch_size':batch_size,
-                        'number_of_epochs':number_of_epochs,
-                        'learning_rate':lr,
-                        'discriminator_learning_rate':discriminator_lr,
-                        'validation_split':validation_split,
-                        'data_augmentation':data_augmentation,
-                        'test_metric_indexes':test_metric_indexes,
-                        'datagen_sampling_pdf':datagen_sampling_pdf
-                        }
-            
-            train_config['dataset_config'] = dataset_config[dataset_name]
-            train_config['model_config'] = model_config[model_name]
-            train_config['optim_config'] = model_config['optim']
-            train_config['other_config'] = model_config['others']
+                        validation_split = 0.1
+                        data_augmentation = ['rotation', 'horizontal_flip', 'vertical_flip']
+                        datagen_sampling_pdf = 0
 
-            model = train_configuration(
-                            data_name=dataset_name, 
-                            train_lr_path=train_lr_path, train_hr_path=train_hr_path, 
-                            val_lr_path=val_lr_path, val_hr_path=val_hr_path,
-                            test_lr_path=test_lr_path, test_hr_path=test_hr_path, 
-                            additional_folder=additional_folder, train_config=train_config,
-                            model_name=model_name, model_configuration=model_config,
-                            verbose=1
-                            )
+                        train_config = {'model':model_name,
+                                    'dataset_name':dataset_name,
+                                    'optimizer':optimizer,
+                                    'discriminator_optimizer':discriminator_optimizer,
+                                    'scheduler':scheduler,
+                                    'discriminator_lr_scheduler':discriminator_lr_scheduler,
+                                    'seed':seed,
+                                    'batch_size':batch_size,
+                                    'number_of_epochs':number_of_epochs,
+                                    'learning_rate':lr,
+                                    'discriminator_learning_rate':discriminator_lr,
+                                    'validation_split':validation_split,
+                                    'data_augmentation':data_augmentation,
+                                    'test_metric_indexes':test_metric_indexes,
+                                    'datagen_sampling_pdf':datagen_sampling_pdf
+                                    }
+                        
+                        train_config['dataset_config'] = dataset_config[dataset_name]
+                        train_config['model_config'] = model_config[model_name]
+                        train_config['optim_config'] = model_config['optim']
+                        train_config['other_config'] = model_config['others']
+
+                        model = train_configuration(
+                                        data_name=dataset_name, 
+                                        train_lr_path=train_lr_path, train_hr_path=train_hr_path, 
+                                        val_lr_path=val_lr_path, val_hr_path=val_hr_path,
+                                        test_lr_path=test_lr_path, test_hr_path=test_hr_path, 
+                                        additional_folder=additional_folder, train_config=train_config,
+                                        model_name=model_name, model_configuration=model_config,
+                                        verbose=1
+                                        )
         else:
             model = train_configuration(
                                     data_name=dataset_name, 
